bcf036b3c074aabfd99f063b6d41ce88e8bf3ca0
Evgeniy.Chukreev <dechukor@gmail.com>
2025-04-23 13:57:40 +0500

    Выполнено подключение и настройка JSON-Server

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { URL } from "./constants/prodURL";
 
 function App() {
-  const [count, setCount] = useState(0)
+  type ProdType = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+  };
+  const [prod, setProd] = useState<ProdType[] | null>(null);
+
+  async function fetchProdId(id: number) {
+    try {
+      const response = await fetch(`${URL}/?id=${id}`);
+
+      const data = await response.json();
+      setProd(data);
+      console.log(prod);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchProdId(1);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{prod && prod[0].title}</div>
+      <button onClick={() => fetchProdId(2)}>BUTTON</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
