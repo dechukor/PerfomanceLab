d8623fd9d74e962dbf8598903d16e4d573cb39ea
Evgeniy.Chukreev <dechukor@gmail.com>
2025-05-04 12:18:26 +0500

    корректировка ссылок

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
